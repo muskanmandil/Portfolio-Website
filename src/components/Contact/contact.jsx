08ad0c2a3d03013610b0d703dd6591f26c611c13
Muskan Mandil <muskan05mandil@gmail.com>
2023-12-27 14:45:36 +0530

    Linked Emailjs services successfully

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import emailjs, { send } from "@emailjs/browser";
 
 import Walmart from '../../assets/walmart.png'
 import Adobe from '../../assets/adobe.png'
@@ -12,6 +13,21 @@ import InstagramIcon from '../../assets/instagram.png'
 
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mfo8z85', 'template_gih0kag', form.current, 'CLBeMmpUYu_Lm5lvR')
+      .then((result) => {
+        console.log(result.text);
+        e.target.reset();
+        alert('Email sent !');
+      }, (error) => {
+        console.log(error.text)
+      });
+  };
+
   return (
     <section id="contactPage">
       <div id="clients">
@@ -30,9 +46,9 @@ const Contact = () => {
       <div id="contact">
         <h1 className="contactPageTitle">Contact Me</h1>
         <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
-        <form className="contactForm">
-            <input type="text" className="name" placeholder="Your Name"/>
-            <input type="email" className="email" placeholder="Your Email" />
+        <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <input type="text" className="name" placeholder="Your Name" name="from_name"/>
+            <input type="email" className="email" placeholder="Your Email" name="from_email" />
             <textarea className='msg' name="message" rows="5" placeholder="Your Message"></textarea>
             <button type='submit' value='send' className="submitBtn">Submit</button>
             <div className="links">
