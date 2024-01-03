964ae72b9fe7496124bd88686512a60747e8de33
Muskan Mandil <muskan05mandil@gmail.com>
2024-01-03 13:53:20 +0530

    all projects description updated

--- src/all_projects.js
+++ src/all_projects.js
@@ -1,4 +1,5 @@
 import GoogleKeep from './assets/GoogleKeep.png'
+import CurriculumDesignPortal from './assets/curriculumDesignPortal.png'
 import Game2048 from './assets/2048Game.png'
 import todoList from './assets/todoList.png'
 import DrumKit from './assets/DrumKit.png'
@@ -13,15 +14,23 @@ const all_projects = [
         liveLink: "https://muskanmandil.github.io/Google-Keep-Clone",
         title: "Google Keep Clone",
         repoLink: "https://github.com/muskanmandil/Google-Keep-Clone",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "Web Clone of Google Keep Notes Application. It has the features of Note Taking, Edit them and Delete them.",
         tech: "HTML, CSS, Javascript"
+    },
+    {
+        img: CurriculumDesignPortal,
+        liveLink: "https://www.figma.com/proto/vYlG2GA3juGoQdpqGLxYAN/Curriculum-Design-Portal?page-id=38%3A87&type=design&node-id=206-7566&viewport=37%2C-8%2C0.04&t=BAKnevpzlrE2IcbM-1&scaling=contain&mode=design",
+        title: "Curriculum Design Portal",
+        repoLink: "",
+        desc: "Emerged as the Top 5 teams in the nation-wide Smart India Hackathon 2023. I designed the complete portal after brainstorming ideas with my team.",
+        tech: "UI/UX Design, Figma"
     },
     {
         img: Game2048,
         liveLink: "https://muskanmandil.github.io/2048-Game",
         title: "2048 Game",
         repoLink: "https://github.com/muskanmandil/2048-Game",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "Clone of the very famous 2048 Game.Respond to arrow keys, and update score on every tile merge.",
         tech: "HTML, CSS, Javascript"
     },
     {
@@ -29,15 +38,15 @@ const all_projects = [
         liveLink: "https://muskanmandil.github.io/Todo-List",
         title: "To-do List",
         repoLink: "https://github.com/muskanmandil/Todo-List",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
-        tech: "HTML, CSS, Javascript"
+        desc: "A to-do list application where you can add your todos, tick off them, edit and delete them with beautiful UI.",
+        tech: "HTML, CSS, Javascript, UI/UX Design"
     },
     {
         img: DrumKit,
         liveLink: "https://muskanmandil.github.io/Drum-Kit",
         title: "Drum Kit",
         repoLink: "https://github.com/muskanmandil/Drum-Kit",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "A Drum Kit on which you can try out your beats, accessible from your keyboard. Have fun!!",
         tech: "HTML, CSS, Javascript"
     }, 
     {
@@ -45,7 +54,7 @@ const all_projects = [
         liveLink: "https://muskanmandil.github.io/Adare-Restaurant",
         title: "Restaurant Website",
         repoLink: "https://github.com/muskanmandil/Adare-Restaurant",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "Featuring a fictional Restaurant with Google Maps integrated with theIr location.",
         tech: "HTML, CSS"
     }, 
     {
@@ -53,7 +62,7 @@ const all_projects = [
         liveLink: "https://muskanmandil.github.io/Roar-Bikes-Repair-Website",
         title: "Bike Repair Website",
         repoLink: "https://github.com/muskanmandil/Roar-Bikes-Repair-Website",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "A static website for a Bike Repair Shop containing all their information, services and how to contact them.",
         tech: "HTML, CSS"
     }, 
     {
@@ -61,7 +70,7 @@ const all_projects = [
         liveLink: "https://muskanmandil.github.io/Calculator",
         title: "Calculator",
         repoLink: "https://github.com/muskanmandil/Calculator",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex architecto fugiat voluptate saepe, suscipit sunt nemo! At, tempora saepe",
+        desc: "Basic Calculator app made with inline javascript. It does all the basic operations of a calculator with beautiful UI.",
         tech: "HTML, CSS, Javascript"
     }
 ]
