35da1a1d607ba83e267508479c23a11596260e54
Muskan Mandil <muskan05mandil@gmail.com>
2024-04-01 02:57:59 +0530

    Email service updated

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -2,10 +2,10 @@ import React, { useRef } from "react";
 import "./contact.css";
 import emailjs from "@emailjs/browser";
 
-import linkedin from '../../assets/icons/linkedin.svg'
-import github from '../../assets/icons/github.svg'
-import instagram from '../../assets/icons/instagram.svg'
-import twitter from '../../assets/icons/twitter.svg'
+import linkedin from "../../assets/icons/linkedin.svg";
+import github from "../../assets/icons/github.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import twitter from "../../assets/icons/twitter.svg";
 
 const Contact = () => {
   const form = useRef();
@@ -13,31 +13,78 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_mfo8z85', 'template_gih0kag', form.current, 'CLBeMmpUYu_Lm5lvR')
-      .then((result) => {
-        console.log(result.text);
-        e.target.reset();
-        alert('Email sent !');
-      }, (error) => {
-        console.log(error.text)
-      });
+    // Get form values
+    const formValues = new FormData(form.current);
+    const fromName = formValues.get("from_name").trim();
+    const fromEmail = formValues.get("from_email").trim();
+    const message = formValues.get("message").trim();
+
+    // Check if any of the fields are empty
+    if (!fromName || !fromEmail || !message) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        "service_mfo8z85",
+        "template_gih0kag",
+        form.current,
+        "CLBeMmpUYu_Lm5lvR"
+      )
+
+      .then(
+        () => {
+          e.target.reset();
+          alert("Email sent!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Error");
+        }
+      );
   };
 
   return (
     <section id="contact">
-        <h1 className="contactTitle">Get in Touch</h1>
-        <form className="contactForm" ref={form} onSubmit={sendEmail}>
-            <input type="text" className="name" placeholder="Your Name" name="from_name"/>
-            <input type="email" className="email" placeholder="Your Email" name="from_email" />
-            <textarea className='msg' name="message" rows="5" placeholder="Your Message"></textarea>
-            <button type='submit' value='send' className="sendBtn">Send Message</button>
-            <div className="socialMediaIconWrapper">
-                <a href="https://www.linkedin.com/in/muskan-mandil/"><img src={linkedin} alt="linkedin" className="socialMediaIcon" /></a>
-                <a href="https://github.com/muskanmandil"><img src={github} alt="github" className="socialMediaIcon" /></a>
-                <a href="https://www.instagram.com/muskan.mandil/"><img src={instagram} alt="instagram" className="socialMediaIcon" /></a>
-                <a href="https://twitter.com/_muskan05"><img src={twitter} alt="twitter" className="socialMediaIcon" /></a>
-            </div>
-        </form>
+      <h1 className="contactTitle">Get in Touch</h1>
+      <form className="contactForm" ref={form} onSubmit={sendEmail}>
+        <input
+          type="text"
+          className="name"
+          placeholder="Your Name"
+          name="from_name"
+        />
+        <input
+          type="email"
+          className="email"
+          placeholder="Your Email"
+          name="from_email"
+        />
+        <textarea
+          className="msg"
+          name="message"
+          rows="5"
+          placeholder="Your Message"
+        ></textarea>
+        <button type="submit" value="send" className="sendBtn">
+          Send Message
+        </button>
+        <div className="socialMediaIconWrapper">
+          <a href="https://www.linkedin.com/in/muskan-mandil/">
+            <img src={linkedin} alt="linkedin" className="socialMediaIcon" />
+          </a>
+          <a href="https://github.com/muskanmandil">
+            <img src={github} alt="github" className="socialMediaIcon" />
+          </a>
+          <a href="https://www.instagram.com/muskan.mandil/">
+            <img src={instagram} alt="instagram" className="socialMediaIcon" />
+          </a>
+          <a href="https://twitter.com/_muskan05">
+            <img src={twitter} alt="twitter" className="socialMediaIcon" />
+          </a>
+        </div>
+      </form>
     </section>
   );
 };
