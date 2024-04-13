5489442824616360f7c8b04cd85fd4c5db2ff9f4
Muskan Mandil <106292794+muskanmandil@users.noreply.github.com>
2024-04-13 11:38:15 +0530

    Updated experience section

--- src/all_experience.js
+++ src/all_experience.js
@@ -6,18 +6,21 @@ const all_experience = [{
     logo: reliableai ,
     position: "UI & UX Intern",
     company: "thereliable.ai",
-    duration: "Mar 2024 - Present",
-    summary: ["Designed user-centric AI-driven database chat interface software prioritizing intuitive interaction.",
-        "Implemented frontend and integration for seamless user experience."]
+    duration: "Mar 2024 - Apr 2024",
+    summary: [
+        "Designed user-centric database chat interface software prioritizing intuitive interaction.",
+        "Led end-to-end frontend and integration using APIs and Websockets for seamless user experience.",
+        "Crafted landing page, effectively communicating the value and features of software to potential users."
+    ]
 },{
     logo: sih ,
     position: "Team 8bit Squad [UI/UX & Frontend]",
     company: "Smart India Hackathon 2023",
     duration: "Sep 2023 - Dec 2023",
-    summary: ["Ranked among the Top 5 teams nationwide in Smart India Hackathon 2023.",
-    "Contributed as UI/UX Designer & frontend developer for AICTE’s Curriculum Design Portal (PS 1465).",
-    `Led design ideation sessions and developed a comprehensive portal interface in collaboration with the team. (<a href="https://bit.ly/3IsFLH2" target="_blank">Prototype</a>)`
-    
+    summary: [
+        "Ranked among the Top 5 teams nationwide in Smart India Hackathon 2023.",
+        "Contributed as UI/UX Designer & frontend developer for AICTE’s Curriculum Design Portal (PS 1465).",
+        `Led design ideation sessions and developed a comprehensive portal interface in collaboration with the team. (<a href="https://bit.ly/3IsFLH2" target="_blank">Prototype</a>)`
     ]
 },{
     logo: cohesive ,
